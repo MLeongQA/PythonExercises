8ab413e690d4becf914d6fd9950e27f1a077d594
MLeongQA <MLeong@qa.com>
2021-07-05 17:53:55 +0100

    Committed near.py exercise

---
+++ Easy/near.py
@@ -0,0 +1,24 @@
+def near(string1, string2):
+    if len(string1)-1 != len(string2):
+        return False
+    else:
+        letter_condition = False
+        count = 0
+        for i in string2:
+            if i == string1[count]:
+                count +=1
+            elif i == string1[count+1] and letter_condition == False:
+                count +=2
+                letter_condition = True
+            else:
+                return False
+
+        return True
+
+print(near("reset", "rest"))
+print(near("dragoon", "dragon"))
+print(near("eave", "leave"))
+print(near("sleet", "lets"))
+
+
+
